7a86b025af274818a810a68a9023aec7a37e907f
Anthony <anthony.combes@etu.sorbonne-universite.fr>
2026-01-28 17:22:23 +0100

    premier push TP1.1.3

--- config_TP1_1_1.py
+++ config_TP1_1_1.py
@@ -7,7 +7,7 @@ import arenas
 display_mode = 0
 arena = 1
 position = False 
-max_iterations = 501 #401*500
+max_iterations = 3000 #401*500
 
 # affichage
 
@@ -20,11 +20,11 @@ display_time_stats = True
 
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
-import robot_dumb
+import robot_braitenberg_avoider
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    robots.append(robot_braitenberg_avoider.Robot_player(4, y_center, 0, name="My Robot", team="A"))
     return robots
--- config_TP1_1_3.py
+++ config_TP1_1_3.py
@@ -5,9 +5,9 @@ import arenas
 # general -- first three parameters can be overwritten with command-line arguments (cf. "python tetracomposibot.py --help")
 
 display_mode = 0
-arena = 1
+arena = 3
 position = False 
-max_iterations = 501 #401*500
+max_iterations = 3000 #401*500
 
 # affichage
 
@@ -20,11 +20,12 @@ display_time_stats = True
 
 # initialization : create and place robots at initial positions (returns a list containing the robots)
 
-import robot_dumb
+import robot_hateWall
 
 def initialize_robots(arena_size=-1, particle_box=-1): # particle_box: size of the robot enclosed in a square
     #x_center = arena_size // 2 - particle_box / 2
     y_center = arena_size // 2 - particle_box / 2
     robots = []
-    robots.append(robot_dumb.Robot_player(4, y_center, 0, name="My Robot", team="A"))
+    for i in range(4):
+        robots.append(robot_hateWall.Robot_player(4, arena_size//2-16+i*8, 0, name="My Robot", team="A"))
     return robots
